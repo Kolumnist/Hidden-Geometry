4f6fb4899dafe70c885f18ccb1a5cd367132e74c
Collin <96417832+Kolumnist@users.noreply.github.com>
2025-04-17 13:23:33 +0200

    T C S

--- Assets/MyStuffForNow/SnapFieldController.cs
+++ Assets/MyStuffForNow/SnapFieldController.cs
@@ -1,95 +1,109 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class SnapFieldController : MonoBehaviour
 {
+
+	// THIS CODE SUCKS
+
 	[SerializeField] 
 	private Transform baseTransform;
 
-	GameObject R;
-	GameObject D;
-	GameObject L;
-	GameObject U;
+	private GameObject Right;
+	private GameObject Down;
+	private GameObject Left;
+	private GameObject Up;
 
-	private void CheckForDuplicationRecursive(Transform current, int currentlyFound)
+	// Starting values for square
+	private float distance_Y = 1.2f;
+	private float distance_X = 1.2f;
+
+	private void CheckForDuplicationRecursive(Transform current, int currentlyFoundCount)
 	{
 		foreach (Transform child in current)
 		{
-			if (currentlyFound == 3 || current == this.transform)
+			if (currentlyFoundCount == 3 || current == this.transform)
 			{
 				break;
 			}
 
-			if (R != null && Vector3.Distance(R.transform.position, child.position) < 0.01f)
+			if (Right != null && Vector3.Distance(Right.transform.position, child.position) < 0.1f)
 			{
-				R.SetActive(false);
-				currentlyFound++;
+				Right.SetActive(false);
+				currentlyFoundCount++;
 				continue;
 			}
-			else if (D != null && Vector3.Distance(D.transform.position, child.position) < 0.01f)
+			else if (Down != null && Vector3.Distance(Down.transform.position, child.position) < 0.1f)
 			{
-				D.SetActive(false);
-				currentlyFound++;
+				Down.SetActive(false);
+				currentlyFoundCount++;
 				continue;
 			}
-			else if (L != null && Vector3.Distance(L.transform.position, child.position) < 0.01f)
+			else if (Left != null && Vector3.Distance(Left.transform.position, child.position) < 0.1f)
 			{
-				L.SetActive(false);
-				currentlyFound++;
+				Left.SetActive(false);
+				currentlyFoundCount++;
 				continue;
 			}
-			else if (U != null && Vector3.Distance(U.transform.position, child.position) < 0.01f)
+			else if (Up != null && Vector3.Distance(Up.transform.position, child.position) < 0.1f)
 			{
-				U.SetActive(false);
-				currentlyFound++;
+				Up.SetActive(false);
+				currentlyFoundCount++;
 				continue;
 			}
 
-			CheckForDuplicationRecursive(child, currentlyFound);
+			CheckForDuplicationRecursive(child, currentlyFoundCount);
 		}
 	}
 
 	private void InstantiateAndSetObjects()
 	{
-		//if(name != "Base") baseTransform = GetComponentInParent<SnapFieldController>().baseTransform;
-	
-		if (name != "L")
+		if (Right != null)
 		{
-			R.name = "R";
-			R.transform.SetPositionAndRotation(new Vector3(1.2f, 0, 0), Quaternion.identity);
-			R.transform.localScale = new Vector3(1f, 1f, 1f);
-			R.transform.SetParent(transform, false);
+			Right.name = "Right";
+			Right.transform.SetPositionAndRotation(new Vector3(distance_X, 0, 0), Quaternion.identity);
+			Right.transform.localScale = new Vector3(1f, 1f, 1f);
+			Right.transform.SetParent(transform, false);
 		}
 
-		if (name != "U")
+		if (Down != null)
 		{
-			D.name = "D";
-			D.transform.SetPositionAndRotation(new Vector3(0, -1.2f, 0), Quaternion.identity);
-			D.transform.localScale = new Vector3(1f, 1f, 1f);
-			D.transform.SetParent(transform, false);
+			Down.name = "Down";
+			Down.transform.SetPositionAndRotation(new Vector3(0, -distance_Y, 0), Quaternion.identity);
+			Down.transform.localScale = new Vector3(1f, 1f, 1f);
+			Down.transform.SetParent(transform, false);
 		}
 
-		if (name != "R")
+		if (Left != null)
 		{
-			L.name = "L";
-			L.transform.SetPositionAndRotation(new Vector3(-1.2f, 0, 0), Quaternion.identity);
-			L.transform.localScale = new Vector3(1f, 1f, 1f);
-			L.transform.SetParent(transform, false);
+			Left.name = "Left";
+			Left.transform.SetPositionAndRotation(new Vector3(-distance_X, 0, 0), Quaternion.identity);
+			Left.transform.localScale = new Vector3(1f, 1f, 1f);
+			Left.transform.SetParent(transform, false);
 		}
 
-		if (name != "D")
+		if (Up != null)
 		{
-			U.name = "U";
-			U.transform.SetPositionAndRotation(new Vector3(0, 1.2f, 0), Quaternion.identity);
-			U.transform.localScale = new Vector3(1f, 1f, 1f);
-			U.transform.SetParent(transform, false);
+			Up.name = "Up";
+			Up.transform.SetPositionAndRotation(new Vector3(0, distance_Y, 0), Quaternion.identity);
+			Up.transform.localScale = new Vector3(1f, 1f, 1f);
+			Up.transform.SetParent(transform, false);
 		}
 	}
 
 	public void DestructSnappers()
 	{
 		Debug.Log("Begin Destructing");
+		/*Transform oldestInteractable = GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform;
+		if (oldestInteractable.name.StartsWith("Quad"))
+		{
+			if(this.name == "Right")
+				transform.SetLocalPositionAndRotation(transform.localPosition + new Vector3(0.3f, 0, 0), Quaternion.identity);
+			else if(this.name == "Left")
+				transform.SetLocalPositionAndRotation(transform.localPosition + new Vector3(-0.3f, 0, 0), Quaternion.identity);
+		}*/
 
 		foreach (Transform child in transform)
 		{
@@ -104,46 +118,101 @@ public class SnapFieldController : MonoBehaviour
 
 		// I know this is dumb but it is in my mind necessary as I need the Instantiation before I set any position.
 		// Also I need to duplicate or put this script on another object in the world the entire time, second option however is weird.
-		switch (this.name)
+		Transform oldestInteractable = GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform;
+		Debug.Log(oldestInteractable.name);
+
+		if (oldestInteractable.name.StartsWith("Quad"))
 		{
-			case "Base":
-				R = Instantiate(gameObject);
-				D = Instantiate(gameObject);
-				L = Instantiate(gameObject);
-				U = Instantiate(gameObject);
+			distance_X = 1.5f;
+			switch (this.name)
+			{
+				case "Base":
+					Right = Instantiate(gameObject);
+					//Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					//Up = Instantiate(gameObject);
+					
+					InstantiateAndSetObjects();
+					break;
+				case "Right":
+					Right = Instantiate(gameObject);
+					//Down = Instantiate(gameObject);
+					//Up = Instantiate(gameObject);
 
-				InstantiateAndSetObjects();
-				break;
-			case "R":
-				R = Instantiate(gameObject);
-				D = Instantiate(gameObject);
-				U = Instantiate(gameObject);
+					transform.SetLocalPositionAndRotation(transform.localPosition + new Vector3(0.3f, 0, 0), Quaternion.identity);
+					InstantiateAndSetObjects();
+					break;
+				/*case "D":
+					Right = Instantiate(gameObject);
+					//Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
 
-				InstantiateAndSetObjects();
-				break;
-			case "D":
-				R = Instantiate(gameObject);
-				D = Instantiate(gameObject);
-				L = Instantiate(gameObject);
+					InstantiateAndSetObjects();
+					break;*/
+				case "Left":
+					//Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					//Up = Instantiate(gameObject);
 
-				InstantiateAndSetObjects();
-				break;
-			case "L":
-				D = Instantiate(gameObject);
-				L = Instantiate(gameObject);
-				U = Instantiate(gameObject);
+					transform.SetLocalPositionAndRotation(transform.localPosition + new Vector3(-0.3f, 0, 0), Quaternion.identity);
+					InstantiateAndSetObjects();
+					break;
+				/*case "U":
+					Right = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					Up = Instantiate(gameObject);
 
-				InstantiateAndSetObjects();
-				break;
-			case "U":
-				R = Instantiate(gameObject);
-				L = Instantiate(gameObject);
-				U = Instantiate(gameObject);
+					InstantiateAndSetObjects();
+					break;*/
+				default:
+					break;
+			}
+		}
+		else
+		{
+			distance_X = 1.2f;
 
-				InstantiateAndSetObjects(); 
-				break;
-			default:
-				break;
+			switch (this.name)
+			{
+				case "Base":
+					Right = Instantiate(gameObject);
+					Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					Up = Instantiate(gameObject);
+
+					InstantiateAndSetObjects();
+					break;
+				case "Right":
+					Right = Instantiate(gameObject);
+					Down = Instantiate(gameObject);
+					Up = Instantiate(gameObject);
+
+					InstantiateAndSetObjects();
+					break;
+				case "Down":
+					Right = Instantiate(gameObject);
+					Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+
+					InstantiateAndSetObjects();
+					break;
+				case "Left":
+					Down = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					Up = Instantiate(gameObject);
+
+					InstantiateAndSetObjects();
+					break;
+				case "Up":
+					Right = Instantiate(gameObject);
+					Left = Instantiate(gameObject);
+					Up = Instantiate(gameObject);
+
+					InstantiateAndSetObjects();
+					break;
+				default:
+					break;
+			}
 		}
 		CheckForDuplicationRecursive(transform.root, 0);
 	}
